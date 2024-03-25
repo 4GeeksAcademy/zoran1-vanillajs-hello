20dee2d79c8fe8d80c9d1a446a241c365feefa0d
ZoranKamov <159853668+ZoranKamov@users.noreply.github.com>
2024-03-25 20:09:32 +0000

    zoran-final-excuse-project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let who = ["My uber driver", "My crocodile", "My neighbor", "My grandma"];
+  let action = ["broke", "ate", "bite", "crashed"];
+  let what = ["my phone", "my scooter", "my bottle"];
+  let when = [
+    "this morning",
+    "yesterday before the game",
+    "while i was showering",
+    "before midnight"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
